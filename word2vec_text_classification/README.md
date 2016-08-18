c1a20779e43509a138d47c3c6eb1ec7cbf404e2b
Cuiyong <cuiyong02@taikangamc.com.cn>
2016-08-18 08:03:44 +0000

    add keras model

--- word2vec_text_classification/README.md
+++ word2vec_text_classification/README.md
@@ -55,3 +55,24 @@ Refer: [使用 Word2Vec 进行文本分类](http://xccds1977.blogspot.sg/2015/05
 这个更为简单粗暴，df 中的每个句子，逐词的 vector 直接相加，然后平均化，得到一个句子 vector，维度显然同词向量维度，也是 100
 
 然后塞给 GradientBoostingClassifier 模型进行分类训练和预测
+
+### 算法四 Keras 深度学习
+
+数据准备：
+
+- 先把 df.seg_word 中每个文档转化为 utf-8 编码，然后调用 keras 的 Tokenizer 进行分词 (空格分割，类似英文)
+- Tokenizer 中记录了词表 vocabulary，词频，并通过 texts_to_sequences 方法，把句子中的词转化为词的 index 编号
+- 各个文档的长度不同，故此选定 (max(文档长度) + median(文档长度)) / 2 为 seqlen，把训练集和测试集中的句子化为等长 (padding和截断)
+- 为了便于和 softmax 层的输出比较，通过 keras.utils.np_utils.to_category 把训练集和测试集的标签转化为 one-hot 向量
+
+到此，训练集 X & Y 维度分别为 (训练集句子数 X seqlen) 和 (训练集句子数 X 标签数)
+
+然后 keras 采用了 4 中模型来训练，分别为
+
+1. CNN: Word-Embedding -> Dropout -> Convolution1D -> MaxPooling1D -> Flatten -> Dense -> Dropout -> Relu -> Dense -> Softmax
+2. LSTM: Word-Embedding -> LSTM -> Dense -> Dropout -> Relu -> Dense -> Softmax
+3. CNN+LSTM: Word-Embedding -> Dropout -> Convolution1D -> MaxPooling1D -> LSTM -> Dropout -> Relu -> Dense -> Softmax
+4. Graph:
+    input -> Word-Embedding ---> Convolution1D -> MaxPooling1D -> Flatten ---> Dense(relu) -> Dropout -> Dense(softmax) -> output
+                             |-> Convolution1D -> MaxPooling1D -> Flatten -|
+                             |-> Convolution1D -> MaxPooling1D -> Flatten -|
--- word2vec_text_classification/classify.py
+++ word2vec_text_classification/classify.py
@@ -1,10 +1,17 @@
 # encoding: utf-8
 
+from __future__ import absolute_import
 from news_loader import make_news_data
 import pandas as pd
 import numpy as np
 from sklearn import metrics
 from gensim.models import word2vec
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.embeddings import Embedding
+from keras.layers.recurrent import LSTM
+from keras.layers.convolutional import Convolution1D, MaxPooling1D
+from keras.utils import np_utils
 
 
 def prepare_data():
@@ -170,6 +177,11 @@ def create_bag_of_centroids(wordlist, word_centroid_map, key_cluster):
 def keras_classify(df):
     # 预处理，把 text 中的词转成数字编号
     from keras.preprocessing.text import Tokenizer
+    from keras.preprocessing import sequence
+    from keras.callbacks import EarlyStopping
+    from sklearn.cross_validation import train_test_split
+
+    print "----- Classification by Keras -----"
     max_features = 50000  # 只选最重要的词
     # Tokenizer 只能处理 str，不能处理 unicode
     textraw = map(lambda x: x.encode('utf-8'), df.seg_word.values.tolist())
@@ -182,6 +194,137 @@ def keras_classify(df):
     nb_classes = len(np.unique(df.label.values))
     print "num of features(vocabulary): ", len(token.word_counts)
     print "num of labels: ", nb_classes
+    max_sent_len = np.max([len(s) for s in text_seq])
+    print "max length or document is: ", max_sent_len
+    median_sent_len = np.median([len(s) for s in text_seq])
+    print "median length or document is: ", median_sent_len
+
+    # 这里的 df.label.values 中 values 不能忽略，否则后面 np_utils.to_categorical 时会出错
+    train_X, test_X, train_y, test_y = train_test_split(text_seq, df.label.values, train_size=0.7, random_state=1)
+    # 目前 train_X & test_X 仍然不是等长的，其每行都是一个 document，需要化为等长的矩阵才能训练
+    seqlen = int(max_sent_len / 2 + median_sent_len / 2)
+    X_train = sequence.pad_sequences(train_X, maxlen=seqlen, padding='post', truncating='post')
+    X_test = sequence.pad_sequences(test_X, maxlen=seqlen, padding='post', truncating='post')
+    # 把 y 格式展开为 one-hot，目的是在 nn 的最后采用 softmax
+    Y_train = np_utils.to_categorical(train_y, nb_classes)
+    Y_test = np_utils.to_categorical(test_y, nb_classes)
+
+    model = build_cnn_model(max_features, seqlen, nb_classes)
+    earlystop = EarlyStopping(monitor='val_loss', patience=1, verbose=1)
+    # 训练 10 轮，每轮 mini_batch 为 32，训练完调用 earlystop 查看是否已经 ok
+    model.fit(X_train, Y_train, batch_size=32, nb_epoch=10, validation_split=0.1, callbacks=[earlystop])
+    evaluate(earlystop.model, X_test, Y_test, test_y)
+
+    model = build_lstm_model(max_features, seqlen, nb_classes)
+    model.fit(X_train, Y_train, batch_size=32, nb_epoch=1, validation_split=0.1)
+    evaluate(model, X_test, Y_test, test_y)
+
+    model = build_mixed_model(max_features, seqlen, nb_classes)
+    model.fit(X_train, Y_train, batch_size=32, nb_epoch=1, validation_split=0.1)
+    evaluate(model, X_test, Y_test, test_y)
+
+    graph = build_graph_model(max_features, seqlen, nb_classes)
+    graph.fit({'input': X_train, 'output': Y_train}, nb_epoch=3, batch_size=32, validation_split=0.1)
+    predict = graph.predict({'input': X_test}, batch_size=32)
+    predict = predict['output']
+    classes = predict.argmax(axis=1)
+    acc = np_utils.accuracy(classes, test_y)
+    print('Test accuracy: ', acc)
+
+
+def evaluate(model, X_test, Y_test, test_y):
+    score = model.evaluate(X_test, Y_test, batch_size=32)
+    print("Test score: ", score)
+    classes = model.predict_classes(X_test, batch_size=32)
+    acc = np_utils.accuracy(classes, test_y)  # 这里要用未转化为 one-hot 之前的 y
+    print("Test accuracy: ", acc)
+
+
+def build_cnn_model(max_features, seqlen, nb_classes):
+    # CNN 模型
+    print ('build cnn model ....')
+    model = Sequential()
+    # 词向量嵌入层，输入：词典大小=max_features=50000、词向量大小=100 类似 Word2Vec、文本长度为上面取的 seqlen
+    # NN 的嵌入层也会把每个词转为一个 word-embedding，类似于学习 WOrd2Vec
+    model.add(Embedding(max_features, 100, input_length=seqlen))
+    model.add(Dropout(0.25))
+    # 卷积层，输入：卷积核个数 200，卷积窗口大小 10
+    model.add(Convolution1D(nb_filter=200, filter_length=10, border_mode='valid', activation='relu'))
+    # 池化层，输入：池化窗口大小 50
+    model.add(MaxPooling1D(pool_length=50))
+    model.add(Flatten())
+    # 全连接层，输入：隐藏层神经元个数 50
+    model.add(Dense(50))
+    model.add(Dropout(0.25))
+    model.add(Activation('relu'))
+    # 输出层
+    model.add(Dense(nb_classes))
+    model.add(Activation('softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=["accuracy"])
+    return model
+
+
+def build_lstm_model(max_features, seqlen, nb_classes):
+    print('build lstm model ...')
+    model = Sequential()
+    model.add(Embedding(max_features, 100, input_length=seqlen))
+    model.add(LSTM(100))
+    # 全连接层，输入：隐藏层神经元个数 50
+    model.add(Dense(50))
+    model.add(Dropout(0.25))
+    model.add(Activation('relu'))
+    # 输出层
+    model.add(Dense(nb_classes))
+    model.add(Activation('softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=["accuracy"])
+    return model
+
+
+# cnn + lstm
+def build_mixed_model(max_features, seqlen, nb_classes):
+    print('build mixed model ...')
+    model = Sequential()
+    model.add(Embedding(max_features, 100, input_length=seqlen))
+    model.add(Dropout(0.25))
+    model.add(Convolution1D(nb_filter=200, filter_length=10, border_mode='valid', activation='relu'))
+    model.add(MaxPooling1D(pool_length=50))
+    model.add(LSTM(100))
+    model.add(Dropout(0.25))
+    model.add(Activation('relu'))
+    model.add(Dense(nb_classes))
+    model.add(Activation('softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=["accuracy"])
+    return model
+
+
+def build_graph_model(max_features, seqlen, nb_classes):
+    from keras.models import Graph
+    fw = [2, 10, 5]
+    pool_length = [2, 50, 10]
+    print('build graph model ...')
+    graph = Graph()
+    graph.add_input(name='input', input_shape=(seqlen,), dtype='int')
+    # Embedding
+    graph.add_node(Embedding(max_features, 100, input_length=seqlen), name='embedding', input='input')
+    # 卷积两个字
+    graph.add_node(Convolution1D(nb_filter=200, filter_length=fw[0], activation='relu'), name='conv1', input='embedding')
+    graph.add_node(MaxPooling1D(pool_length=pool_length[0], border_mode='valid'), name='pool1', input='conv1')
+    graph.add_node(Flatten(), name='flat1', input='conv1')  # 为啥不是 pool1 ？？
+    # 卷积 10 个字
+    graph.add_node(Convolution1D(nb_filter=200, filter_length=fw[1], activation='relu'), name='conv2', input='embedding')
+    graph.add_node(MaxPooling1D(pool_length=pool_length[1], border_mode='valid'), name='pool2', input='conv2')
+    graph.add_node(Flatten(), name='flat2', input='conv2')  # 为啥不是 pool2 ？？
+    # 卷积 5 个字
+    graph.add_node(Convolution1D(nb_filter=200, filter_length=fw[2], activation='relu'), name='conv3', input='embedding')
+    graph.add_node(MaxPooling1D(pool_length=pool_length[2], border_mode='valid'), name='pool3', input='conv3')
+    graph.add_node(Flatten(), name='flat3', input='conv3')  # 为啥不是 pool3 ？？
+    # put it all together
+    graph.add_node(Dense(50, activation='relu'), name='dense1', inputs=['flat1', 'flat2', 'flat3'], merge_mode='concat')
+    graph.add_node(Dropout(0.5), name='drop1', input='dense1')
+    graph.add_node(Dense(nb_classes, activation='softmax'), name='softmax', input='drop1')
+    graph.add_output(name='output', input='softmax')
+    graph.compile('Adam', loss={'output': 'categorical_crossentropy'})
+    return graph
 
 
 if __name__ == '__main__':
@@ -191,3 +334,4 @@ if __name__ == '__main__':
     df, model, txtlist = word2vec_transform(df, num_features)
     cluster_classify(model, df, txtlist)
     vectorize_classify(model, df, num_features)
+    keras_classify(df)
