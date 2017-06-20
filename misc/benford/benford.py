eb0e91f6f1727184be69f1523f913a01060cf1db
Cuiyong <cuiyong02@taikangamc.com.cn>
2017-06-20 09:09:39 +0000

    add kstesst/chisqure/confidence-interval measurement

--- misc/benford/benford.py
+++ misc/benford/benford.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+""" reference:
+    https://arxiv.org/pdf/1201.3432.pdf
+"""
+
 from math import log10, floor
 import numpy as np
-from scipy.stats import ks_2samp
 from scipy import spatial
+from scipy.stats import chisquare
 
 
 ideal_distribution = np.array([0.30103, 0.176091, 0.124939, 0.09691, 0.0791812, 0.0669468, 0.0579919, 0.0511525, 0.0457575])
@@ -23,14 +27,33 @@ def pearson_correlation(dist):
     return np.corrcoef(ideal_distribution, dist)[0, 1]
 
 
-def kstest(dist):
-    """ If the K-S statistic is small or the p-value is high, then we cannot
-        reject the hypothesis that the distributions of the two samples are the same.
-        p-value: 发生第一类错误(弃真)的几率，其阈值 α 可取单尾也可取双尾，ks_2samp 只实现了双尾
-        α 一般规定=0.05或=0.01，其意义为：假设检验中如果拒绝时，发生Ⅰ型错误的概率为5%或1%，即100次拒绝的结论中，平均有5次或1次是错误的。
-        这里直接返回 p-value 就好了，K-S 统计量虽然也行，但是不好评判
+def pearson_chisquare(dist, N):
+    """ 对于 8 (9-1) 个自由度，5% significant level，critical value of chi-square is  15.507
+        这里返回 critical value - chi_square statistics
+        如果返回值大于 0，则通过测试，95% 满足 benford's law，且越大越好；否则，未通过测试
     """
-    return ks_2samp(ideal_distribution, dist).pvalue
+    return 15.507 - chisquare(dist, ideal_distribution)[0] * N
+
+
+def confidence_intervals(counts, N):
+    """ 对于 5% 置信区间，z-value = 1.96
+        返回观察值在 1~9 都落在置信区间的个数；越大越好，9 表示全部数字都落在置信区间
+    """
+    cis = 1.96 * np.sqrt((ideal_distribution * (1 - ideal_distribution)) / N) * 100
+    ideal_counts = ideal_distribution * N
+    return len([i for i, c in enumerate(counts) if c >= (ideal_counts[i] - cis[i]) and c <= (ideal_counts[i] + cis[i])])
+
+
+def kstest(counts, N):
+    """ 不要使用 scipy.stats.ks_2samp，结果不对
+        按 http://epublications.bond.edu.au/cgi/viewcontent.cgi?article=1004&context=mike_steele 这个计算
+        对于 5% significant level，critical value equals to 1.36   [by formula: cv(alpha) = sqrt(-0.5 * ln(alpha / 2))
+        对于样本总数 N, 如果 ks-statistics > critical value / N，则拒绝 H0 假设，分布不满足 benford law
+        故此，我们这里返回 (critical value / N) - ks-statistics
+        如果返回值大于 0，则通过测试，95% 满足 benford's law，且越大越好；否则，未通过测试
+    """
+    cv = 1.36 / np.sqrt(N)
+    return cv - np.max(np.abs(np.array([np.sum((counts - N * ideal_distribution)[0:i + 1]) for i in range(9)]))) / N
 
 
 def cosine_similarity(dist):
